aa62332a18ce3c8fc12bd988853b4746f774d517
WadeHarris <97800110+WadeHarris007@users.noreply.github.com>
2024-02-02 09:18:52 +0200

    commit 1

---
+++ server.js
@@ -0,0 +1,42 @@
+//implement express
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+//creating a folder and file to store our data
+const mongodb = require('./data/database');
+
+const app = express();
+
+//setting port to 6000
+const port = process.env.PORT || 6000;
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  })
+
+//this is to include the folder routes when we need code from routes
+app.use('/', require('./routes'));
+
+
+
+
+
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    
+    }
+    else {
+        app.listen(port, () => {
+            console.log(`Database and node is running on port ${port})`)});
+    }
+
+});
